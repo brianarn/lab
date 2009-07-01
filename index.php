ca8d0027e44521941e6e4411564de0c8d59520b5
Brian Arnold <brianarn@gmail.com>
2009-06-30 19:09:52 -0700

    Adding in a few more pieces

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+		<title>HTML5 Drum Kit</title>
+		<!-- CSS -->
+		<link type="text/css" rel="stylesheet" href="styles/reset.css" />
+		<link type="text/css" rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/redmond/jquery-ui.css" />
+		<!-- Scripts -->
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7/jquery.min.js"></script>
+		<!--[if lte IE 8]>
+		<script src="js/html5.js" type="text/javascript"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<header>
+			<h1>Brian's Lab</h1>
+		</header>
+		<section id="content">
+			<p>For now, here's a list of stuff under here. Total mess right now. Organizing soon.</p>
+			<ul>
+				<li><a href="canvas">Canvas</a></li>
+				<li><a href="html5drums">HTML 5 Drum Kit</a></li>
+			</ul>
+		</section>
+		<footer>
+			<p>HTML5 Drum Kit by Brian Arnold, All Rights Reserved for now</p>
+		</footer>
+		<script type="text/javascript">
+			// Run on DOM ready
+			$(document).ready(function(){
+			});
+		</script>
+	</body>
+</html>
