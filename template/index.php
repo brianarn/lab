f8526ad712e46fdf74eb373bf0e1af498feb8f08
Brian Arnold <brianarn@gmail.com>
2009-07-27 20:03:21 -0700

    Adding in a generic template for use with later labs, eventually to be obsoleted by EE

---
+++ template/index.php
@@ -0,0 +1,43 @@
+<?
+/*
+ * <NAME HERE>
+ * Copyright (c) 2009 Brian Arnold
+ * Software licensed under MIT license, see http://www.randomthink.net/lab/LICENSE
+ */
+?><!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+		<title>#NAMEHERE# | RandomThink Labs</title>
+		<link type="text/css" rel="stylesheet" href="../styles/reset.css" />
+		<link type="text/css" rel="stylesheet" href="../styles/lab.css" />
+		<link type="text/css" rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/redmond/jquery-ui.css" />
+		<link type="text/css" rel="stylesheet" href="template.css" />
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7/jquery-ui.min.js"></script>
+		<!--[if lte IE 8]>
+		<script src="../js/html5.js" type="text/javascript"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<header>
+			<h1>#NAMEHERE#</h1>
+		</header>
+		<section id="content">
+		</section> <!-- #content -->
+		<footer>
+			<p>#NAMEHERE# by <a href="http://www.randomthink.net/">Brian Arnold</a>, MIT licensed so snag away</p>
+		</footer>
+		<script type="text/javascript" src="template.js"></script>
+		<!-- Analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-279164-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>	
+	</body>
+</html>
---
+++ template/template.js
@@ -0,0 +1,13 @@
+/*
+ * <NAME HERE>
+ * Copyright (c) 2009 Brian Arnold
+ * Software licensed under MIT license, see http://www.randomthink.net/lab/LICENSE
+ */
+
+// !===== VARIABLES =====
+
+// !===== FUNCTIONS =====
+
+// !===== DOM READY =====
+$(document).ready(function(){
+}); // $(document).ready(...)
