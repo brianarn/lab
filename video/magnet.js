4cd2f0231dd27cac215c5d6feafc4f8e81329e15
Brian Arnold <brianarn@gmail.com>
2010-04-22 14:30:27 -0700

    Starting up an experiment with video

---
+++ video/magnet.js
@@ -0,0 +1,14 @@
+/*
+ * magnet.js
+ * Copyright Brian Arnold 2010
+ * MIT license, meaning you can do whatever
+ *
+ * Inspired by craftymind.com article, to achieve
+ * an effect from my childhood that isn't so easily
+ * achieved anymore
+ */
+
+// Give a hoot, don't pollute (the global space)
+(function(window,document,undefined){
+})(window,document);
+// vim: set sw=2 ts=2 et:
