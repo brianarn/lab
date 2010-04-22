e9cdcec4734489b6115eb48e12dbc552dd80d921
Brian Arnold <brianarn@gmail.com>
2010-04-22 15:32:32 -0700

    Checkpoint

--- video/magnet.js
+++ video/magnet.js
@@ -20,14 +20,22 @@
     , resctx   // Result canvas 2d context
     , rtimer   // Video to canvas interval
     , dtimer   // Canvas altering interval
-    ;
+  ;
+
+  const
+    FPS = 30 // General framerate
+  ;
 
   // Functions
 
-  // 
+  // refreshVid: Just write the video to the base
+  function refreshVid() {
+    basectx.drawImage(vid,0,0);
+  } // refreshVid
 
   // Events
   document.addEventListener('DOMContentLoaded', function(e) {
+
     console.log("DOMContentLoaded");
 
     // Get some DOM references
@@ -35,14 +43,44 @@
     base = document.getElementById('base');
     cursor = document.getElementById('cursor');
     result = document.getElementById('result');
+    basectx = base.getContext('2d');
+    curctx = cursor.getContext('2d');
+    resctx = result.getContext('2d');
 
+    // Hook some events onto the video
     vid.addEventListener('loadedmetadata', function(e) {
-      console.log("Video metadata loaded");
+      var w, h;
+
+      console.log("vid loadedmetadata");
+
+      // Using the metadata, resize some canvases
+      w = vid.videoWidth;
+      h = vid.videoHeight;
+      base.width    = w;
+      base.height   = h;
+      result.width  = w;
+      result.height = h;
     }, false); // vid loadedmetadata
 
+    // Set some events on the video
     vid.addEventListener('play', function(e) {
       // Video is playing, so kick up our refresher
-    }, false);
+      console.log("vid play");
+      rtimer = setInterval(refreshVid, 1000/FPS);
+    }, false); // vid play
+
+    vid.addEventListener('pause', function(e) {
+      // Video is being paused, so stop the refreshser
+      console.log("vid pause");
+      clearInterval(rtimer);
+    }, false); // vid pause
+
+    // Exposing the video globally for testing on my part
+    window.vid = vid;
   }, false); // DOMContentLoaded
+
+  document.addEventListener('mousemove', function(e) {
+    console.log("MOUSEMOVED");
+  }, false); // DOM onmousemove
 })(window,document);
 // vim: set sw=2 ts=2 et:
