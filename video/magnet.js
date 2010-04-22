7e9ed17213c45ee664acc7690226a21b9c203bcc
Brian Arnold <brianarn@gmail.com>
2010-04-22 15:14:01 -0700

    Checkpoint

--- video/magnet.js
+++ video/magnet.js
@@ -10,5 +10,39 @@
 
 // Give a hoot, don't pollute (the global space)
 (function(window,document,undefined){
+  // Variables
+  var vid      // Video DOM reference
+    , base     // Base canvas DOM ref
+    , basectx  // Base canvas 2d context
+    , cursor   // Cursor canvas DOM ref
+    , curctx   // Cursor canvas 2d context
+    , result   // Result canvas DOM ref
+    , resctx   // Result canvas 2d context
+    , rtimer   // Video to canvas interval
+    , dtimer   // Canvas altering interval
+    ;
+
+  // Functions
+
+  // 
+
+  // Events
+  document.addEventListener('DOMContentLoaded', function(e) {
+    console.log("DOMContentLoaded");
+
+    // Get some DOM references
+    vid = document.getElementById('src');
+    base = document.getElementById('base');
+    cursor = document.getElementById('cursor');
+    result = document.getElementById('result');
+
+    vid.addEventListener('loadedmetadata', function(e) {
+      console.log("Video metadata loaded");
+    }, false); // vid loadedmetadata
+
+    vid.addEventListener('play', function(e) {
+      // Video is playing, so kick up our refresher
+    }, false);
+  }, false); // DOMContentLoaded
 })(window,document);
 // vim: set sw=2 ts=2 et:
