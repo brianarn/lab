1da6848df2984f9c706a04dd38b1ccafd64f2ed9
Brian Arnold <brianarn@gmail.com>
2010-07-20 13:15:10 -0700

    Adding a template for Dojo play

---
+++ dojo/template.js
@@ -0,0 +1,12 @@
+// #TEMPLATE#
+// Brian Arnold
+// MIT licensed, though I doubt you'd actually want to take any of it yet.
+
+// Dojo Requires
+
+// Ready, do somethin'
+dojo.ready(function(){
+}); // dojo.ready
+
+// Vim settings, whee
+// vim: set sw=4 ts=4 noet:
